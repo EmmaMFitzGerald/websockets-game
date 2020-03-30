fec5ecc511ad971ea3c23914c1885e75845aa4d8
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-03-30 09:17:43 -0400

    connection through websockets made

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import * as express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+    res.sendFile(path.resolve("./dist/public/index.html"))
+);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const http = require("http").Server(app);
+
+const server = app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`)
+);
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const io = socketio(server);
+
+io.on("connection", socket => {
+    console.log("made connection");
+
+    socket.emit("message", "welcome to the app");
+})
+
+app.use(express.static("public"));
